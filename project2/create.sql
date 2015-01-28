b3ec8e29bfecd338e6776f1059124c76aa3a6844
stackedqueue <yang.julian@gmail.com>
2015-01-27 21:36:20 -0800

    Created create.sql, which creates the four tables for our database

---
+++ project2/create.sql
@@ -0,0 +1,46 @@
+CREATE TABLE Users
+                (   
+                    UserID INTEGER, 
+                    BuyerRating INTEGER, 
+                    SellerRating Integer,
+                    Location VARCHAR(200), 
+                    Country VARCHAR(200),
+                    PRIMARY KEY (UserID)
+                );
+
+CREATE TABLE Items 
+                (   
+                    ItemID INTEGER, 
+                    Name VARCHAR(200), 
+                    BuyPrice DECIMAL(8,2),
+                    FirstBid DECIMAL(8,2), 
+                    Started TIMESTAMP, 
+                    Ends TIMESTAMP, 
+                    Latitude VARCHAR(200), 
+                    Longitude VARCHAR(200), 
+                    Location VARCHAR(200), 
+                    Country VARCHAR(200), 
+                    Description VARCHAR(4000), 
+                    SellerID INTEGER,
+                    PRIMARY KEY (ItemID), 
+                    FOREIGN KEY (SellerID) REFERENCES Users(UserID)
+                );
+
+CREATE TABLE ItemCategories
+                (
+                    ItemID Integer,
+                    Category VARCHAR(200),
+                    PRIMARY KEY (ItemID, Category),
+                    FOREIGN KEY (ITEMID) REFERENCES Items(ItemID)
+                );
+
+CREATE TABLE Bids
+                (
+                    BidderID Integer,
+                    ItemID Integer,
+                    Time TIMESTAMP,
+                    Amount DECIMAL(8,2),
+                    PRIMARY KEY (BidderID, ItemID, Time),
+                    FOREIGN KEY (BidderID) REFERENCES Users(UserID),
+                    FOREIGN KEY (ItemID) REFERENCES Items(ItemID)
+                );
