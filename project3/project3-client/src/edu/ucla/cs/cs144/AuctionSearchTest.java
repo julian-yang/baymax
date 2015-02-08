e3f9c33d0f76a016fa6269ee2d02ae1c83ec63ce
stackedqueue <yang.julian@gmail.com>
2015-02-07 20:38:37 -0800

    Setup part C and part D

--- project3/project3-client/src/edu/ucla/cs/cs144/AuctionSearchTest.java
+++ project3/project3-client/src/edu/ucla/cs/cs144/AuctionSearchTest.java
@@ -3,21 +3,20 @@ package edu.ucla.cs.cs144;
 import java.util.Calendar;
 import java.util.Date;
 
-import edu.ucla.cs.cs144.AuctionSearch;
-import edu.ucla.cs.cs144.SearchRegion;
+import edu.ucla.cs.cs144.AuctionSearchClient;
 import edu.ucla.cs.cs144.SearchResult;
+import org.apache.axis2.AxisFault;
 
 public class AuctionSearchTest {
 	public static void main(String[] args1)
 	{
-		AuctionSearch as = new AuctionSearch();
         
 		String message = "Test message";
-		String reply = as.echo(message);
+		String reply = AuctionSearchClient.echo(message);
 		System.out.println("Reply: " + reply);
 		
 		String query = "superman";
-		SearchResult[] basicResults = as.basicSearch(query, 0, 20);
+		SearchResult[] basicResults = AuctionSearchClient.basicSearch(query, 0, 20);
 		System.out.println("Basic Search Query: " + query);
 		System.out.println("Received " + basicResults.length + " results");
 		for(SearchResult result : basicResults) {
@@ -25,30 +24,30 @@ public class AuctionSearchTest {
 		}
 		
 		// superman, number of basic results
-		basicResults = as.basicSearch("superman", 0, 9999);
+		basicResults = AuctionSearchClient.basicSearch("superman", 0, 9999);
 		System.out.println("Received " + basicResults.length + " results for superman");
 		
 		// kitchenware, number of basic results
-		basicResults = as.basicSearch("kitchenware", 0, 9999);
+		basicResults = AuctionSearchClient.basicSearch("kitchenware", 0, 9999);
 		System.out.println("Received " + basicResults.length + " results for kitchenware");
 
 		// star trek, number of basic results
-		basicResults = as.basicSearch("star trek", 0, 9999);
+		basicResults = AuctionSearchClient.basicSearch("star trek", 0, 9999);
 		System.out.println("Received " + basicResults.length + " results for star trek");
 
 		// camera, number of basic results
-		basicResults = as.basicSearch("camera", 0, 9999);
+		basicResults = AuctionSearchClient.basicSearch("camera", 0, 9999);
 		System.out.println("Received " + basicResults.length + " camera");
 		
 		// camera, print first 3 results
-		basicResults = as.basicSearch("camera", 0, 3);
+		basicResults = AuctionSearchClient.basicSearch("camera", 0, 3);
 		System.out.println("Received " + basicResults.length + " camera");
 		for(SearchResult result : basicResults) {
 			System.out.println(result.getItemId() + ": " + result.getName());
 		}
 		
 		// camera, print 3 results, skipping first 2
-		basicResults = as.basicSearch("camera", 2, 3);
+		basicResults = AuctionSearchClient.basicSearch("camera", 2, 3);
 		System.out.println("Received " + basicResults.length + " camera");
 		for(SearchResult result : basicResults) {
 			System.out.println(result.getItemId() + ": " + result.getName());
@@ -57,7 +56,7 @@ public class AuctionSearchTest {
 		// spatial search on camera
 		SearchRegion region =
 		    new SearchRegion(33.774, -118.63, 34.201, -117.38); 
-		SearchResult[] spatialResults = as.spatialSearch("camera", region, 0, 20);
+		SearchResult[] spatialResults = AuctionSearchClient.spatialSearch("camera", region, 0, 20);
 		System.out.println("Spatial Search");
 		System.out.println("Received " + spatialResults.length + " results");
 		for(SearchResult result : spatialResults) {
@@ -66,7 +65,7 @@ public class AuctionSearchTest {
 		
 		// xml test
 		String itemId = "1043495702";
-		String item = as.getXMLDataForItemId(itemId);
+		String item = AuctionSearchClient.getXMLDataForItemId(itemId);
 		System.out.println("XML data for ItemId: " + itemId);
 		System.out.println(item);
 
