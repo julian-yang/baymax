d84a8ce8891f7c62d4ce1394e78f7b67135d3e4a
Nathan Tung <nathanctung@gmail.com>
2015-02-22 23:09:43 -0800

    Implemented item servlet (item lookup by ID)

--- project4/src/edu/ucla/cs/cs144/ItemServlet.java
+++ project4/src/edu/ucla/cs/cs144/ItemServlet.java
@@ -11,8 +11,23 @@ public class ItemServlet extends HttpServlet implements Servlet {
        
     public ItemServlet() {}
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
-        // your codes here
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // get parameters for basic search
+		String id = preventNullString(request.getParameter("id"));
+		
+		// get XML data of item
+		String itemXML = AuctionSearchClient.getXMLDataForItemId(id);
+		
+		request.setAttribute("result", itemXML);
+		
+		request.getRequestDispatcher("/item.jsp").forward(request, response);
     }
+	
+	// given a string, return empty string if it is null; otherwise, return as is
+	public String preventNullString(String str) {
+		if(str==null) {
+			return "";
+		}
+		return str;
+	}
 }
