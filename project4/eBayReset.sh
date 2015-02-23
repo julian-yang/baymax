492f3a8f5609e143dd424773f15aab9367638b48
Nathan Tung <nathanctung@gmail.com>
2015-02-22 22:46:33 -0800

    Added shell script to automate .war refresh process

---
+++ project4/eBayReset.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# remove eBay.war and eBay directory in webapps folder
+rm -rf $CATALINA_BASE/webapps/eBay*
+
+# use ant build on project4
+cd /home/cs144/shared/baymax/project4
+ant build
+
+# copy new eBay.war file to webapps
+cp /home/cs144/shared/baymax/project4/build/eBay.war $CATALINA_BASE/webapps
