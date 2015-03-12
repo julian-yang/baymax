5f56b10120746904b24d6ef2d27e6e83f7091933
Nathan Tung <nathanctung@gmail.com>
2015-03-12 15:25:28 -0700

    Fixed confirmation servlet

--- project5/src/edu/ucla/cs/cs144/ConfirmServlet.java
+++ project5/src/edu/ucla/cs/cs144/ConfirmServlet.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.FactoryConfigurationError;
@@ -18,12 +17,15 @@ import org.w3c.dom.Element;
 import org.xml.sax.InputSource;
 import java.io.StringReader;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class ConfirmServlet extends HttpServlet implements Servlet {
     
     public ConfirmServlet() {}
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession(true);
         Object item = session.getAttribute("ItemResult");
         request.setAttribute("ItemResult", item);
